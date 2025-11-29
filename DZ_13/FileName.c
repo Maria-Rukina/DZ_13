5de739ed0bf99008efd2714b5b0ef70f3e4cbad0
user <user@WIN-EDG70QA3MEF>
2025-11-29 18:21:56 +0300

    Добавьте файлы проекта.

---
+++ DZ_13/FileName.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void main()
+{
+    system("chcp 1251");
+
+    char str[100];
+
+    printf("Введите строку: ");
+    //ели ели бобров
+    fgets(str, sizeof(str), stdin); 
+    str[strcspn(str, "\n")] = '\0';  // убрать \n
+
+    char* words[50];    // до 50 слов
+    int n = 0;          //счёт кол-во слов
+
+    char* token = strtok(str, " \t");  // делим на слова по пробелам/табам
+    while (token != NULL && n < 50) 
+    {
+        words[n++] = token;
+        token = strtok(NULL, " \t");
+    }
+
+    // Меняем местами указатели на первое и последнее слово
+    char* temp = words[0];
+    words[0] = words[n - 1];
+    words[n - 1] = temp;
+
+    // Выводим
+    for (int i = 0; i < n; i++) 
+    {
+        if (i > 0) printf(" ");
+        printf("%s", words[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
